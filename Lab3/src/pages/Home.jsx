df453ebaa9eba8d96050883709ef562cd8980734
myduckperson <illarionov0405@gmail.com>
2024-12-27 18:39:41 +0200

    add basic pages to apply protected routes to

---
+++ Lab3/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home(){
+
+    return <div>
+        <h1>Home!</h1>
+        <h2>No secret info here!</h2>
+    </div>
+}
---
+++ Lab3/src/pages/SecretPage.jsx
@@ -0,0 +1,7 @@
+export default function SecretPage(){
+
+    return <div>
+        <h1>Secret page!</h1>
+        <h2>Do not share this with anyone!</h2>
+    </div>
+}
