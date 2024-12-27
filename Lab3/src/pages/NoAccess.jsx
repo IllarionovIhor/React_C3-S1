20b4905c61d23b13b7dbadb0dc5c44931a8712db
myduckperson <illarionov0405@gmail.com>
2024-12-27 19:00:47 +0200

    finishing touches

--- Lab3/src/pages/NoAccess.jsx
+++
@@ -1,7 +0,0 @@
-export default function NoAccess(){
-
-    return <div>
-        <h1>Access restricted!</h1>
-        <h2>You are not allowed to see that!</h2>
-    </div>
-}
--- Lab3/src/pages/SecretPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function SecretPage(){
-
-    return <div>
-        <h1>Secret page!</h1>
-        <h2>Do not share this with anyone!</h2>
-    </div>
-}
